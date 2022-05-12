8076bb545689a072cd14a86a67874b1e83ab13e7
Bao Vo Gia <62041598+giabaohb48@users.noreply.github.com>
2022-05-12 15:04:26 +0700

    deploy

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -13,4 +13,6 @@ export const config = {
   'jwt': {
     'secret': process.env.JWT_SECRET,
   },
+  'access_key_id': process.env.AWS_ACCESS_KEY_ID,
+  'secret_access_key': process.env.AWS_SECRET_ACCESS_KEY,
 };
