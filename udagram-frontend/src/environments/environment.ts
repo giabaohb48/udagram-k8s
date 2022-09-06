d06baeb998c2f725ebf3250a15e0bdc1d7e8e1dd
giabaohb48 <giabaovo123@gmail.com>
2022-09-06 13:27:50 +0700

    not build frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-export const external_ip = process.env.EXTERNAL_IP
+var external_ip = process.env.EXTERNAL_IP
 export const environment = {
   production: false,
   appName: 'Udagram',
